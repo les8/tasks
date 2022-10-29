67a4994bc462faad7e989695ee99c83fb56412e9
Leonid Skurikhin <leonid.skurihin@yandex.ru>
2022-10-29 14:27:51 +0300

    App component #1

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,27 @@
 <template>
-  <div class="wrapper"></div>
+  <div class="wrapper">
+    <TheSidebar />
+    <TheTable />
+  </div>
 </template>
 
 <script>
+import TheSidebar from "./components/sidebar/TheSidebar.vue";
+import TheTable from "./components/table/TheTable.vue";
+
 export default {
   name: "App",
+  components: {
+    TheSidebar,
+    TheTable,
+  },
 };
 </script>
 
-<style scoped></style>
+<style lang="scss">
+@import "./style/nullstyle.scss";
+
+.wrapper {
+  display: flex;
+}
+</style>
